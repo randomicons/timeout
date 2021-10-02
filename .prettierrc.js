525032fd649942b4e21c1cd7e4275912998c4d80
Sriram Bhat <srirambhat7@gmail.com>
2021-10-02 13:44:17 -0500

    eslint

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,6 +2,7 @@ module.exports = {
   bracketSpacing: false,
   jsxBracketSameLine: true,
   singleQuote: true,
+  semi: false,
   trailingComma: 'all',
   arrowParens: 'avoid',
-};
+}
--- App.js
+++ App.js
@@ -6,28 +6,22 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
+import type {Node} from 'react'
+import React from 'react'
 import {
+  Button,
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
   View,
-} from 'react-native';
+} from 'react-native'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen'
 
 const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === 'dark'
   return (
     <View style={styles.sectionContainer}>
       <Text
@@ -49,46 +43,37 @@ const Section = ({children, title}): Node => {
         {children}
       </Text>
     </View>
-  );
-};
+  )
+}
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === 'dark'
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  }
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="Timeout">
+            <Button title={'Join Lobby'} onClick={() => {}} />
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
+          <Section>
+            <Button title={'Create Lobby'} onClick={() => {}} />
           </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          {/*<LearnMoreLinks />*/}
         </View>
       </ScrollView>
     </SafeAreaView>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   sectionContainer: {
@@ -107,6 +92,6 @@ const styles = StyleSheet.create({
   highlight: {
     fontWeight: '700',
   },
-});
+})
 
-export default App;
+export default App
