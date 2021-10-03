cd0b2429fd92894d142feb7fcdb4c48671bfdec3
Sriram Bhat <srirambhat7@gmail.com>
2021-10-02 22:31:11 -0500

    joinlobby button

--- pages/CreateLobby.js
+++ pages/CreateLobby.js
@@ -1,8 +1,12 @@
-import { Button, StyleSheet, TextInput, View } from 'react-native'
-import React, { useState } from 'react'
+import {Button, StyleSheet, TextInput, View} from 'react-native'
+import React, {useState} from 'react'
+import axios from 'axios'
+import {uuid} from 'uuidv4'
+import {useHistory} from 'react-router-native'
 
 const CreateLobby = () => {
   const [password, setPassword] = useState('')
+  const history = useHistory()
   return (
     <View>
       <TextInput
@@ -12,7 +16,17 @@ const CreateLobby = () => {
         placeholder="password"
         secureTextEntry
       />
-      <Button title={'Create'} />
+      <Button
+        title={'Create'}
+        onClick={async () => {
+          history.push(
+            await axios.post(
+              'https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/timeout-aezpk/service/join_lobby/incoming_webhook/webhook0',
+              {userId: uuid()},
+            ),
+          )
+        }}
+      />
     </View>
   )
 }
